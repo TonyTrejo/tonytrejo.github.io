9eb7b3d2ecd7e02b032596ffa567a4fe93b58ef6
tonytrejo <you@example.com>
2018-05-22 17:20:36 -0700

    added asteroid game

---
+++ Asteroid.js
@@ -0,0 +1,40 @@
+function Asteroid(x, y, size, color) {
+
+    this.position = createVector(x, y);
+
+    this.size = size; // radius
+    this.color = color;
+}
+
+/**
+ * directs asteroid toward ship
+ * checks collision with ship
+ */
+Asteroid.prototype.update = function () {
+
+    var path = createVector(width / 2, height / 2).sub(this.position); // figure out path
+    path.setMag(5 - log(this.size)); // change magnitude based upon size of asteroid
+
+    this.position.add(path); // change position based upon path
+
+    /* check collision with ship */
+    var d = dist(this.position.x, this.position.y, width / 2, height / 2);
+
+    if (d < this.size / 2) {
+        // distance is greater than the raidus
+
+        endGame();
+    }
+};
+
+/**
+ * draws asteroid
+ */
+Asteroid.prototype.draw = function () {
+
+    fill(51);
+    stroke(this.color);
+    strokeWeight(5);
+
+    ellipse(this.position.x, this.position.y, this.size);
+};
---
+++ Lazer.js
@@ -0,0 +1,58 @@
+function Lazer(angle, radius, speed) {
+
+    /* cartesian coordinates */
+    this.x = null;
+    this.y = null;
+
+    /* polar coordinates */
+    this.angle = angle; // theta
+    this.radius = radius; // size
+
+    this.speed = speed; // speed
+
+    this.onScreen = true;
+}
+
+/**
+ * calculates new position
+ * converts that position to cartesian coordinates
+ * updates on-screen state
+ */
+Lazer.prototype.update = function () {
+
+    /* change position */
+    this.radius += this.speed;
+
+    /* convert to cartesian */
+    this.x = this.radius * sin(this.angle);
+    this.y = this.radius * cos(this.angle);
+
+    /* update on-screen state */
+    this.onScreen = (this.radius < width);
+};
+
+/**
+ * returns whether the Lazer hits asteroid
+ */
+Lazer.prototype.penetrates = function (asteroid) {
+
+    var d = dist(this.x + width / 2, this.y + height / 2, asteroid.position.x, asteroid.position.y);
+
+    return (d < asteroid.size / 2);
+};
+
+/**
+ * draws the Lazer
+ */
+Lazer.prototype.draw = function () {
+
+    stroke("#009900");
+    strokeWeight(3);
+
+    push(); // save translations
+
+    translate(width / 2, height / 2);
+    point(this.x, this.y);
+
+    pop(); // revert translations
+};
